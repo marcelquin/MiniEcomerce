f9c29ec8e141efa86fcf9a604e14a400819586e7
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2024-06-05 18:48:47 -0400

    git

--- frontend/src/Pagina/Pedido.js
+++ frontend/src/Pagina/Pedido.js
@@ -7,7 +7,7 @@ function Pedido() {
     const [APIData, setAPIData] = useState([]);
     useEffect(() => {
         Axios
-          .get("http://34.69.39.159:8080/pedido/ListarPedidos")
+          .get("http://130.211.116.164:8080/pedido/ListarPedidos")
           .then((response) => { setAPIData(response.data)})
           .catch((err) => {
             console.error("ops! ocorreu um erro" + err);
