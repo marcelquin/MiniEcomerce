d64550b0e7f40148602d7917caaa92db833d8d49
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2024-06-06 10:56:35 -0400

    git backend

--- backend/src/main/java/App/Security/Util/SecurityConfigurations.java
+++ backend/src/main/java/App/Security/Util/SecurityConfigurations.java
@@ -38,7 +38,7 @@ public class SecurityConfigurations {
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
+    
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
