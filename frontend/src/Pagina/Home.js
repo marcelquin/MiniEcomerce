d0dca7409bcbc3eb7313814d5a059b95a34b1c9f
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2024-06-06 11:04:20 -0400

    git  front

--- frontend/src/Pagina/Home.js
+++ frontend/src/Pagina/Home.js
@@ -8,7 +8,7 @@ function Home() {
 
     useEffect(() => {
         Axios
-          .get("http://130.211.116.164:8080/pedido/ListarPedidosAbertos")
+          .get("http://104.198.154.70:8080/pedido/ListarPedidosAbertos")
           .then((response) => { setAPIData(response.data)})
           .catch((err) => {
             console.error("ops! ocorreu um erro" + err);
@@ -20,7 +20,7 @@ function Home() {
 
       async function NovoPedido(e){
         try{
-          fetch('http://34.29.169.201:8080/pedido/NovoPedido', {
+          fetch('http://104.198.154.70:8080/pedido/NovoPedido', {
             method: 'POST',
             headers:{
               'Content-Type': 'application/x-www-form-urlencoded'
@@ -40,7 +40,7 @@ function Home() {
 
       async function AdicionarProduto(e){
         try{
-          fetch('http://34.29.169.201:8080/pedido/AdicionarProdutoPedido', {
+          fetch('http://104.198.154.70:8080/pedido/AdicionarProdutoPedido', {
             method: 'PUT',
             headers:{
               'Content-Type': 'application/x-www-form-urlencoded'
@@ -60,7 +60,7 @@ function Home() {
       
       useEffect(() => {
         Axios
-          .get("http://130.211.116.164:8080/pedido/ListarPedidosAbertos")
+          .get("http://104.198.154.70:8080/pedido/ListarPedidosAbertos")
           .then((response) => { setAPIData(response.data)})
           .catch((err) => {
             console.error("ops! ocorreu um erro" + err);
@@ -69,7 +69,7 @@ function Home() {
 
       async function FinalizarPedido(e){
         try{
-          fetch('http://34.29.169.201:8080/pedido/FinalizarPedido', {
+          fetch('http://104.198.154.70:8080/pedido/FinalizarPedido', {
             method: 'PUT',
             headers:{
               'Content-Type': 'application/x-www-form-urlencoded'
--- frontend/src/Pagina/Pedido.js
+++ frontend/src/Pagina/Pedido.js
@@ -7,7 +7,7 @@ function Pedido() {
     const [APIData, setAPIData] = useState([]);
     useEffect(() => {
         Axios
-          .get("http://34.29.169.201:8080/pedido/ListarPedidos")
+          .get("http://104.198.154.70:8080/pedido/ListarPedidos")
           .then((response) => { setAPIData(response.data)})
           .catch((err) => {
             console.error("ops! ocorreu um erro" + err);
