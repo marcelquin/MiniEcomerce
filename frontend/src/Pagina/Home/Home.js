aa8dd54bb86cc6b0f5c253daaecbf1646d84e4e1
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2024-06-25 21:07:58 -0400

    git

--- frontend/src/Pagina/Home/Home.js
+++ frontend/src/Pagina/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const baseUrl = "http://34.133.121.3:8080"
+  const baseUrl = "http://34.136.115.180:8080"
   //const baseUrl = "http://localhost:8080"
 
     const [APIData, setAPIData] = useState([]);
